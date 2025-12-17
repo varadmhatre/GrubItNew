f0e090c179b6d8bd4145190d37db931ce5f993b0
VaradMhatre <113052651+varadmhatre@users.noreply.github.com>
2025-12-17 14:53:32 +0530

    Remove sensitive Firebase config details
    Removed Firebase configuration details from firebase-config.js.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,17 +3,12 @@
 // Replace with your Firebase web app config
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCR01N7CSB7OfB8VxHzkV725Zeo7ct-ibA",
-  authDomain: "grubit-45d09.firebaseapp.com",
-  projectId: "grubit-45d09",
-  storageBucket: "grubit-45d09.firebasestorage.app",
-  messagingSenderId: "787109161650",
-  appId: "1:787109161650:web:bb9a0f8f321f8a75bb77ab",
-  measurementId: "G-3P7M922KET"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+
 
